e31d1560980bf8ed918f34176b3d44abff845522
William Hilska <whilska@gmail.com>
2018-05-03 21:44:11 -0400

    new source distribution

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
         name="groupmewebsocket",
         author="William Hilska",
         author_email="whilska@gmail.com",
-        version="1.0.5",
+        version="1.0.6",
         py_modules=["groupmewebsocket"]
 )
