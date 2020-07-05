1652b60a69a594fb88b5daff68ba4c9b33847fce
William Hilska <whilska@gmail.com>
2020-07-05 17:36:50 -0400

    fixed dependency issue

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         name='groupmewebsocket',
         author='William Hilska',
         author_email='whilska@gmail.com',
-        version='1.0.7',
+        version='1.0.8',
         py_modules=['groupmewebsocket'],
-        install_requires=['websocket', 'requests']
+        install_requires=['websocket-client', 'requests']
 )
